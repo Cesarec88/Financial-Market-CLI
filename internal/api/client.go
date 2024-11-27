f0023462e1def584576043c25869f008cb46fdca
Cesare <cesarec88@gmail.com>
2024-11-27 17:42:03 +0000

    feat(client): add client logic

---
+++ internal/api/client.go
@@ -0,0 +1,43 @@
+package api
+
+import (
+	"encoding/json"
+	"financial-market-cli/internal/config"
+	"fmt"
+	"net/http"
+)
+
+type Client struct {
+	APIKey     string
+	BaseURL    string
+	HTTPClient *http.Client
+}
+
+func NewClient(cfg *config.Config) *Client {
+	return &Client{
+		APIKey:     cfg.APIKey,
+		BaseURL:    cfg.BaseURL,
+		HTTPClient: &http.Client{},
+	}
+}
+
+func (c *Client) GetStockQuote(symbol string) (map[string]interface{}, error) {
+	url := fmt.Sprintf("%s?function=GLOBAL_QUOTE&symbol=%s&apikey=%s", c.BaseURL, symbol, c.APIKey)
+	resp, err := c.HTTPClient.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("error making API request: %v", err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	var result map[string]interface{}
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return nil, fmt.Errorf("error decoding API response: %v", err)
+	}
+
+	return result, nil
+}
